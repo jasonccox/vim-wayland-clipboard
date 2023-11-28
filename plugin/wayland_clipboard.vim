jasonccox/vim-wayland-clipboard

commit bf10e1d84f9e3154ef9aba1ec6ec219892e30b8e

Make put mappings easier to work with
Functions are much easier to re-use and require less confusing escaping.

plugin/wayland_clipboard.vim
@@ -60,15 +60,28 @@ augroup END
 " Pasting {{{
 
 " remap paste commands to first pull in clipboard contents with wl-paste
-let prepaste = "silent let @\"=substitute(system('wl-paste --no-newline'), \"\\r\", '', 'g')"
 
-for p in ['p', 'P']
-    execute "nnoremap <silent> \"+" . p . " :<C-U>" . prepaste . " \\| exec 'normal! ' . v:count1 . '" . p . "'<CR>"
-endfor
+function! s:clipboard_to_unnamed()
+    silent let @"=substitute(system('wl-paste --no-newline'), "\r", '', 'g')
+endfunction
 
-for cr in ['<C-R>', '<C-R><C-R>', '<C-R><C-O>', '<C-R><C-P>']
-    execute "inoremap <silent> " . cr . "+ <C-O>:<C-U>" . prepaste . "<CR>" . cr . "\""
-endfor
+function! s:put(p)
+    call s:clipboard_to_unnamed()
+    return '""' . a:p
+endfunction
+
+function! s:ctrl_r(cr)
+    call s:clipboard_to_unnamed()
+    return cr . '"'
+endfunction
+
+nnoremap <expr> <silent> "+p <SID>put('p')
+nnoremap <expr> <silent> "+P <SID>put('P')
+
+nnoremap <expr> <silent> <C-R>+ <SID>ctrl_r("\<C-R>")
+nnoremap <expr> <silent> <C-R><C-R>+ <SID>ctrl_r("\<C-R>\<C-R>")
+nnoremap <expr> <silent> <C-R><C-O>+ <SID>ctrl_r("\<C-R>\<C-O>")
+nnoremap <expr> <silent> <C-R><C-P>+ <SID>ctrl_r("\<C-R>\<C-P>")
 
 " }}}
 

commit 15bbf52a85a691cc8c5b825c8607724fcce13f96

Fix <C-R>+ and friends in insert mode
Fixes: bf10e1d (Make put mappings easier to work with)

plugin/wayland_clipboard.vim
@@ -83,7 +83,7 @@ endfunction
 
 function! s:ctrl_r(cr)
     call s:clipboard_to_unnamed()
-    return cr . '"'
+    return a:cr . '"'
 endfunction
 
 nnoremap <expr> <silent> "+p <SID>put('p', v:false)
@@ -91,10 +91,10 @@ nnoremap <expr> <silent> "+P <SID>put('P', v:false)
 nnoremap <expr> <silent> p <SID>put('p', &clipboard != 'unnamedplus')
 nnoremap <expr> <silent> P <SID>put('P', &clipboard != 'unnamedplus')
 
-nnoremap <expr> <silent> <C-R>+ <SID>ctrl_r("\<C-R>")
-nnoremap <expr> <silent> <C-R><C-R>+ <SID>ctrl_r("\<C-R>\<C-R>")
-nnoremap <expr> <silent> <C-R><C-O>+ <SID>ctrl_r("\<C-R>\<C-O>")
-nnoremap <expr> <silent> <C-R><C-P>+ <SID>ctrl_r("\<C-R>\<C-P>")
+inoremap <expr> <silent> <C-R>+ <SID>ctrl_r("\<C-R>")
+inoremap <expr> <silent> <C-R><C-R>+ <SID>ctrl_r("\<C-R>\<C-R>")
+inoremap <expr> <silent> <C-R><C-O>+ <SID>ctrl_r("\<C-R>\<C-O>")
+inoremap <expr> <silent> <C-R><C-P>+ <SID>ctrl_r("\<C-R>\<C-P>")
 
 " }}}
 